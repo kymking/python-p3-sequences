fd2629348c0198c465da757c08ab7a1da17e3129
kymking <kimaniantidote655@gmail.com>
2025-12-05 11:53:04 +0300

    commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < length:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+
+    print(fib_sequence)
